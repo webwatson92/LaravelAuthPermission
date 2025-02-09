4139ea0f962399f857a85772c03d7ea5c17ac70e
webwatson92 <webwatson92@gmail.com>
2025-02-09 17:43:30 +0000

    Mise à jour de composer

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -275,8 +275,8 @@ class ComposerStaticInitf7faa92cf4d4a504bdb737233b71fa4f
         ),
         'Psr\\Http\\Message\\' => 
         array (
-            0 => __DIR__ . '/..' . '/psr/http-message/src',
-            1 => __DIR__ . '/..' . '/psr/http-factory/src',
+            0 => __DIR__ . '/..' . '/psr/http-factory/src',
+            1 => __DIR__ . '/..' . '/psr/http-message/src',
         ),
         'Psr\\Http\\Client\\' => 
         array (
@@ -320,9 +320,9 @@ class ComposerStaticInitf7faa92cf4d4a504bdb737233b71fa4f
         ),
         'Illuminate\\Support\\' => 
         array (
-            0 => __DIR__ . '/..' . '/illuminate/conditionable',
-            1 => __DIR__ . '/..' . '/illuminate/macroable',
-            2 => __DIR__ . '/..' . '/illuminate/collections',
+            0 => __DIR__ . '/..' . '/illuminate/collections',
+            1 => __DIR__ . '/..' . '/illuminate/conditionable',
+            2 => __DIR__ . '/..' . '/illuminate/macroable',
             3 => __DIR__ . '/..' . '/illuminate/support',
         ),
         'Illuminate\\Session\\' => 
